82675f600fe01a42a5212df86d808ab12f2017f6
af-bsu <aidenfreeman@u.boisestate.edu>
2026-09-17 15:16:31 -0600

    Starting point; checking args are provided

---
+++ FormatChecker.java
@@ -0,0 +1,13 @@
+
+public class FormatChecker {
+    public static void main(String[] args) {
+        // Don't waste my program's time.
+        if (args.length < 1) {
+            System.out.println("No arguments (valid file names) provided.");
+            System.out.println("Usage: $ java FormatChecker file1 [file2 file3 ... fileN]");
+            System.exit(1);
+        }
+
+        
+    }
+}

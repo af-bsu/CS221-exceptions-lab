5129daaebb102a34c99ccb93ec53189de7970728
af-bsu <aidenfreeman@u.boisestate.edu>
2026-09-17 15:49:38 -0600

    Not a correct solution; retrying later after a break

--- FormatChecker.java
+++ FormatChecker.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
 
 public class FormatChecker {
+    private String delimiter = "\\s+";
+
+    // Utility methods go here
+    public boolean checkFileExists(String filename) throws FileNotFoundException {
+        try {
+            File testFile = new File(filename);
+        } catch (FileNotFoundException e) {
+            e.toString();
+            return false;
+        }
+
+        return true;
+    }
+
+    public boolean checkTable(File file) {
+        int rows, cols;
+        Scanner linescan = new Scanner(file);
+        linescan.close();
+    }
+
     public static void main(String[] args) {
         // Don't waste my program's time.
         if (args.length < 1) {
@@ -8,6 +29,14 @@ public class FormatChecker {
             System.exit(1);
         }
 
-        
+        int fileIndex = 0;
+        while (fileIndex < args.length) {
+            try {
+                File file = new File(args[fileIndex]);
+            } catch (FileNotFoundException e) {
+                e.toString();
+                System.out.println("INVALID\n");
+            }
+        }
     }
 }
